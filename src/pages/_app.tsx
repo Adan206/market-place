a9f973e83e1cc6df3b0d07f01b3d37c3c6b5fa58
adan206 <adanweli@hotmail.com>
2023-04-12 17:43:35 -0700

    added navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { NavBar } from "~/components/navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <NavBar />
       <Component {...pageProps} />;
     </ClerkProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
+import { NavBar } from "~/components/navbar";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -17,16 +18,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user.isSignedIn ? (
-          <SignOutButton>
-            <span className="text-white">Sign Out</span>
-          </SignOutButton>
-        ) : (
-          <SignInButton>
-            <span className="text-white">Sign in</span>
-          </SignInButton>
-        )}
-
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
